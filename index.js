897872be6cbc1528d98f5f90cc32a9953f3e0b0c
juan cuadros <juan.cuadros@eia.edu.co>
2019-12-13 23:29:45 -0500

    parsing request paths

--- index.js
+++ index.js
@@ -6,10 +6,24 @@
 
  //dependencies
  var http = require('http');
+ var url = require('url');
 
 //the server should respond to all requests with a string
  var server = http.createServer(function(req,res){
+
+//get and parse the URL
+   var parsedUrl = url.parse(req.url,true);
+
+//get the path
+   var path = parsedUrl.pathname;
+   var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+//send response
    res.end('Hello World\n');
+
+//log requested path
+   console.log('request received on path: '+trimmedPath);
+
  });
 
  //start the server, listening on port 3000
