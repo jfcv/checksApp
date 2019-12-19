5a072261ec4b493e3a69d72b3054b7cb07337982
juan cuadros <juan.cuadros@eia.edu.co>
2019-12-18 23:02:25 -0500

    routing requests

--- index.js
+++ index.js
@@ -37,12 +37,37 @@
   req.on('end',function(){
     buffer += decoder.end();
 
-    //send response
-      res.end('Hello World\n');
+    //choose the handler this request should go to. If one is not found it should go to the notFound handler
+    var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
-    //log requested path
-      console.log('request received with this payload:',buffer);
+    //construct data object to send to the handler
+    var data = {
+      'trimmedPath' : trimmedPath,
+      'queryStringObject' : queryStringObject,
+      'method' : method,
+      'headers' : headers,
+      'payload' : buffer
+    };
 
+    //route the request to the handler specified in the router
+    chosenHandler(data,function(statusCode,payload){
+
+      //use the status code called back by the handler, or default to 200
+        statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+      //use the payload called back by the handler, or default to an empty object
+        payload = typeof(payload) == 'object' ? payload : {};
+
+      //convert the payload to a string
+      payloadString = JSON.stringify(payload);
+
+      //return the response
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      //log the responde
+      console.log('returning this response: ',statusCode,payloadString);
+    });
   });
  });
 
@@ -50,3 +75,22 @@
  server.listen(3000,function(){
    console.log('server listening on port 3000');
  });
+
+//define the handlers
+ var handlers = {};
+
+//sample handler
+ handlers.sample = function(data,callback){
+   //callback an HTTP status code, and a payload object
+   callback(406,{'name' : 'sample handler'})
+ };
+
+//not found handler
+handlers.notFound = function(data,callback){
+  callback(404);
+};
+
+//define a request router
+ var router = {
+   'sample' : handlers.sample
+ };
