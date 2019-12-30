f66ffbb6ff166fd8bb56e8253b1baf1aebc561b9
juan cuadros <juan.cuadros@eia.edu.co>
2019-12-30 15:52:13 -0500

    tokens route added, /tokens library added, tokens handlers container added, tokens POST working

--- index.js
+++ index.js
@@ -106,5 +106,6 @@ var unifiedServer = function(req,res){
 //define a request router
  var router = {
    'ping' : handlers.ping,
-   'users' : handlers.users
+   'users' : handlers.users,
+   'tokens' : handlers.tokens
  };
